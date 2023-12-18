9634d568ec4b45787e015f765fda633d69114a2a
oussamaZAAM <zaam.oussama@gmail.com>
2023-12-18 20:47:28 +0100

    Add a pipeline for build + test

--- src/test/java/com/url/shortener/integration/config/TestMongoConfig.java
+++ src/test/java/com/url/shortener/integration/config/TestMongoConfig.java
@@ -2,6 +2,7 @@ package com.url.shortener.integration.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,9 +10,18 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @TestConfiguration
 public class TestMongoConfig {
 
+    @Value("${spring.data.mongodb.host}")
+    private int mongoHost;
+
+    @Value("${spring.data.mongodb.port}")
+    private int mongoPort;
+
+    @Value("${spring.data.mongodb.testdatabase}")
+    private int database;
+
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017/testdatabase"); // Specify your test database URL
+        return MongoClients.create("mongodb://"+mongoHost+":"+mongoPort+"/"+database); // Specify your test database URL
     }
 
     @Bean
